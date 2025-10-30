336d82c7a325f3438f0d7a1e997dc4dca6d096bc
fmenebro <f.menebroecker@smail.uni-koeln.de>
2025-10-30 21:01:28 +0100

    Create H3_main.java

---
+++ h3/H3_main.java
@@ -0,0 +1,25 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        int i = 210;
+        int j = 102;
+        int k = -10;
+
+        if (i > j) {
+            if (i > 200) {
+                if (j > 100) {
+                    k = 3;
+                } else {
+                    k = 2;
+                }
+            } else if (!(j > 100)) {
+                k = 1;
+            }
+        } else if (!(i > j) && !(i > 200) && !(j > 100)) {
+            k = 4;
+        }
+
+        System.out.println("k = " + k);
+    }
+}
